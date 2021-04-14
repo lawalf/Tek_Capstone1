cae73891a40a3192e52759f7596b5fce69142365
lawalf <funmi2lawal@gmail.com>
2021-04-14 09:07:40 -0400

    Sixth Push

--- info.php
+++ info.php
@@ -1,11 +1,11 @@
 <?php
 
-$connect = mysql_connect(“server_name”, “admin_name”, “password”);
+$connect = mysql_connect(Local Connection 1, root, JigglyPuff1234);
 if (!connect) { 
-    die('Connection Failed: ' . mysql_error()); { mysql_select_db(“database_name”, $connect);
+    die('Connection Failed: ' . mysql_error()); { mysql_select_db(“tractordb”, $connect);
     }
 
-$user_info = “INSERT INTO table_name (username, email) VALUES ('$_POST[username]', '$_POST[email]')”;
+$user_info = “INSERT INTO sales_info (tractor, product code, manufacturer, extended service plan) VALUES ('$_POST[tractor]', '$_POST[product code]', '$_POST[manufacturer]', '$_POST[extended service plan]',)”;
 if (!mysql_query($user_info, $connect)) {
      die('Error: ' . mysql_error()); 
     }

107731c6868ce6601e2490a1c090f2273c652d4f
lawalf <funmi2lawal@gmail.com>
2021-04-14 09:09:38 -0400

    Seventh Push

--- capture_store_data_capstone1.sql
+++ capture_store_data_capstone1.sql
@@ -1,5 +1,5 @@
-#DROP SCHEMA IF EXISTS da_bootcamp_capstone1_lawal; 
-#CREATE SCHEMA IF NOT EXISTS da_bootcamp_capstone1_lawal;
+DROP SCHEMA IF EXISTS da_bootcamp_capstone1_lawal; 
+CREATE SCHEMA IF NOT EXISTS da_bootcamp_capstone1_lawal;
 USE da_bootcamp_capstone1_lawal;
 
 SELECT * FROM Sales_Team;
